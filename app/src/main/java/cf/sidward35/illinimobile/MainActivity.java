0d2369f8000b179093a0351bdf60fd56826a07e9
sidward35 <sid@stscamps.org>
2018-08-01 19:01:10 +0530

    Update icon and add 'Loading...' indicator

--- app/src/main/java/cf/sidward35/illinimobile/MainActivity.java
+++ app/src/main/java/cf/sidward35/illinimobile/MainActivity.java
@@ -2,6 +2,8 @@ package cf.sidward35.illinimobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -11,10 +13,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
         setContentView(R.layout.activity_main);
         WebView builder = findViewById(R.id.webview);
         WebSettings webSettings = builder.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        getWindow().setFeatureInt( Window.FEATURE_PROGRESS, Window.PROGRESS_VISIBILITY_ON);
+        builder.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress){
+                setTitle("Loading...");
+                setProgress(progress * 100);
+                if(progress == 100) setTitle(R.string.app_name);
+            }
+        });
         builder.loadUrl("https://go.illinois.edu/studentmobile");
         builder.setWebViewClient(new WebViewClient());
     }
